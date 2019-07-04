6e60de58be848c955b61f7b98ed3bee973500ce6
Ralph deGennaro <rmdegennaro@gmail.com>
2019-07-04 19:56:51 -0400

    start of new project

---
+++ android/app/src/main/kotlin/com/rmdegennaro/rio_flutter_playground01/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rmdegennaro.rio_flutter_playground01
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
